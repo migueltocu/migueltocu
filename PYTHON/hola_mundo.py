247f86d3d2fcd9f17ef0d37591b387a16cddef5b
migueltocu <mitorrescuello@gmail.com>
2024-10-22 22:06:22 +0200

    Create hola_mundo.py

---
+++ PYTHON/hola_mundo.py
@@ -0,0 +1,9 @@
+# 1. Ejercicios Sesión 1
+#   1. Crea un archivo Python que muestre por consola "¡Hola, Mundo!"
+#   2. Ahora modifícalo para que muestre "¡Hola, [Nombre]" donde el nombre es el valor (str) de una variable
+
+
+
+print("¡Hola, Mundo!")
+Nombre="Miguel"
+print(f"¡Hola, {Nombre}!")
